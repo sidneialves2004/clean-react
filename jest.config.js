c9fb253bfedd3978ed7c505cb6e3c7b918796113
Sidnei Alves <sidneialves2004@yahoo.com.br>
2022-01-20 09:47:24 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
