3462ef188de6c90abb8af444b4afdfafed6f6066
Sidnei Alves <sidneialves2004@yahoo.com.br>
2022-04-09 16:58:05 -0300

    feat: create Authentication UseCase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,9 @@
+import { AccountModel } from '../models/AccountModel'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+export interface Authentication {
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
+}
