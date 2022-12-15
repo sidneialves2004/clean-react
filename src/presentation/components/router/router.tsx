7538c93950629b85f498166252fcf57f6fd041a5
Sidnei Alves <sidneialves2004@yahoo.com.br>
2022-12-15 09:03:13 -0300

    feat: ensure LoginComponent will not load spinner an error on start

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,11 +2,13 @@ import { Login } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import '@/presentation/styles/global.scss'
+import Start from '@/presentation/pages/start/start'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Start />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
