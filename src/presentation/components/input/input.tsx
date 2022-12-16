5d5ab2b7cd91866df8e89ffbc24394562e6eb546
Sidnei Alves <sidneialves2004@yahoo.com.br>
2022-12-16 10:08:42 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -6,10 +6,16 @@ import PropTypes from 'prop-types'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
   }
   const getStatus = (): string => {
     return '🔴'
@@ -17,9 +23,10 @@ const Input: React.FC<Props> = (props: Props) => {
   const getTitle = (): string => {
     return error
   }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input data-testid={props.name} {...props} readOnly onFocus={enableInput} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
