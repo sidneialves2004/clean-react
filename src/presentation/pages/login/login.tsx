b4e2b1714f3ef781b64dca3f5f067f0a62ac82f4
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-10 11:30:07 -0300

    feat: ensure Login present error if Authentication fails

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -11,6 +11,7 @@ type Props = {
 }
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+  // const history = useHistory()
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -23,33 +24,42 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email',state.email),
+      emailError: validation.validate('email', state.email),
       passwordError: validation.validate('password', state.password)
     })
-  },[state.email, state.password])
+  }, [state.email, state.password])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
-    if (state.isLoading || state.emailError || state.passwordError) return
-    setState({ ...state, isLoading: true })
-    await authentication.auth({
-      email: state.email,
-      password: state.password
-    })
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) {
+        return
+      }
+      setState({ ...state, isLoading: true })
+      await authentication.auth({
+        email: state.email,
+        password: state.password
+      })
+    } catch (error) {
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: error.message
+      })
+    }
   }
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={{ state, setState }}>
-          <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
-            <h2>Login</h2>
-            <Input type="email" name="email" placeholder='Digite seu e-mail'/>
-            <Input type="password" name="password" placeholder='Digite sua senha'/>
-            <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type='submit'>Entrar</button>
-            <span className={Styles.link}>Criar Conta</span>
-            <FormStatus />
-          </form>
+      <Context.Provider value={ { state, setState }}>
+        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Entrar</button>
+          <FormStatus />
+        </form>
       </Context.Provider>
       <Footer />
     </div>
