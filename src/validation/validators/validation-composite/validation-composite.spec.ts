3d5ade7b4357f3a91ab488e9b5f9cbdf47f56e81
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-12 10:14:38 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { FieldValidationSpy } from '@/validation/validators/test'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const validationSpy = new FieldValidationSpy('any_field')
+    validationSpy.error = new Error('first_error_message')
     const validationSpy2 = new FieldValidationSpy('any_field')
-    validationSpy2.error = new Error('any_error_message')
+    validationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
       validationSpy,
       validationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
