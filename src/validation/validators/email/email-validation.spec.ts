04dd790cde0e5d265be54feefd41c3055a632286
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-12 08:22:20 -0300

    test: ensure EmailValidation return falsy  if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from '@/validation/validators'
+import faker from 'faker'
 
 describe('Email Validation', () => {
   test('should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError('email'))
+  })
+
+  test('should return falsy if email is valid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
