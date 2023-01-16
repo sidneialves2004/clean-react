4e65b8617490c50cf459647cd6fe0aad35ab3603
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-16 09:19:56 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios/axios-http-client.spec.ts
+++ src/infra/http/axios/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 
-import { mockAxios } from '@/infra/test'
+import { mockAxios, mockHttpResponse } from '@/infra/test'
 import axios from 'axios'
 import { mockPostRequest } from '@/data/test'
 import { AxiosHttpClient } from '@/infra/http'
@@ -30,6 +30,15 @@ describe('AxiosHttpClient', () => {
 
   test('should return the correct statusCode and body', () => {
     const { sut, mockedAxios } = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  })
+
+  test('should return the correct statusCode and body on failure', () => {
+    const { sut, mockedAxios } = makeSut()
+    mockedAxios.post.mockRejectedValueOnce({
+      response: mockHttpResponse()
+    })
     const promise = sut.post(mockPostRequest())
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
   })
