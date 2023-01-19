437f354c2d7eabeb763312a86c0eff3a64fbf50b
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-19 20:08:43 -0300

    refactor: rename class SaveAccessToken

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,7 +1,7 @@
 import { SetStorage } from '@/data/protocols/cache'
-import { SaveAccessTokenn } from '@/domain/usecases'
+import { SaveAccessToken } from '@/domain/usecases'
 
-export class LocalSaveAccessToken implements SaveAccessTokenn {
+export class LocalSaveAccessToken implements SaveAccessToken {
   constructor (private readonly setStorage: SetStorage) {}
   async save (accessToken: string): Promise<void> {
     await this.setStorage.set('accessToken', accessToken)
--- src/domain/usecases/save-access-token.ts
+++ src/domain/usecases/save-access-token.ts
@@ -1,3 +1,3 @@
-export interface SaveAccessTokenn {
+export interface SaveAccessToken {
   save: (accessToken: string) => Promise<void>
 }
