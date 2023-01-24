27d30e17c1d0aa4f86e8b1db89b1e50240adfd66
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-01-24 19:01:48 -0300

    test: ensure Signup show valid name state if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -67,6 +67,12 @@ describe('Signup Component', () => {
     Helper.testStatusForField(sut,'passwordConfirmation', validationError)
   })
 
+  test('should show valid name state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut,'name')
+  })
+
   test('should show valid email state if Validation succeeds', () => {
     const { sut } = makeSut()
     Helper.populateField(sut, 'email')
