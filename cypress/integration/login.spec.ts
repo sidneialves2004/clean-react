402824f00cdbba0a6013489e02755ab18b04df85
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-05 16:41:34 -0300

    test: ensure login begin with correct initial state

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('should behave...', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
