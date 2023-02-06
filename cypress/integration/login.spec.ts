bcfc5cba33353797730c6ab29ae3319f7eae9039
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-06 08:32:27 -0300

    test: ensure Login present error state if form is invalid

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,14 +1,31 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    cy.getByTestId('password').should('have.attr', 'readOnly')
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
       .should('contain.text', '🔴')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo email inválido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo password deve ter no minimo 5 caracteres')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
