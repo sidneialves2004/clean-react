4edbe891dc9b88c4b5beff3578bc3a15d242bece
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-09 10:37:24 -0300

    test: ensure Signup present UnexpectedError if invalid data is returned

--- cypress/support/signup-mocks.ts
+++ cypress/support/signup-mocks.ts
@@ -1,4 +1,6 @@
 import * as Helper from './http-mocks'
+import faker from 'faker'
 
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError('signup')
 export const mockUnexpectedError = (): void => Helper.mockUnexpectedError('POST', 'signup')
+export const mockInvaldData = (): void => Helper.mockOk('POST', 'signup', { invalidProperty: faker.random.uuid() })
