e0c9571d13645f95f4e8a2a9c165c3dea77881a6
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-21 11:59:06 -0300

    refactor: remove validation accessToken

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,7 @@
-import { UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { makeLocalstorageAdapter } from '@/main/factories/cache'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
   makeLocalstorageAdapter().set('account', account)
 }
 
