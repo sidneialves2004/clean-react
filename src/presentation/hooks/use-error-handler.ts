7c17d03fd0c75e124a15a5b91731afb81fd9ba96
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-23 07:03:47 -0300

    refactor: add useLogout to remove code duplication

--- src/presentation/hooks/use-error-handler.ts
+++ src/presentation/hooks/use-error-handler.ts
@@ -1,18 +1,14 @@
-import { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import { ApiContext } from '@/presentation/contexts'
+import { useLogout } from '@/presentation/hooks'
 import { AccessDiniedError } from '@/domain/errors'
 
 type CallBackType = (error: Error) => void
 type ResultType = CallBackType
 
 export const useErrorHandler = (callback: CallBackType): ResultType => {
-  const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const logout = useLogout()
   return (error: Error): void => {
     if (error instanceof AccessDiniedError) {
-      setCurrentAccount(undefined)
-      history.replace('/login')
+      logout()
     } else {
       callback(error)
     }
