82203c2c2e9ee87daee1b251573acd534eacc1ba
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-23 09:25:40 -0300

    test: ensure SurveyList logout on AccessDiniedError

--- cypress/integration/survey-list.spec.ts
+++ cypress/integration/survey-list.spec.ts
@@ -5,11 +5,17 @@ import * as Http from '../support/survey-list-mocks'
 describe('SurveyList', () => {
   beforeEach(() => {
     Helper.setLocalStorageItem('account', { accessToken: faker.random.uuid(), name: faker.name.findName() })
-    cy.visit('')
   })
 
   it('should present error on UnexpectedError', () => {
     Http.mockUnexpectedError()
+    cy.visit('')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+  })
+
+  it('should logout on ForbiddenError', () => {
+    Http.mockAccessDiniedError()
+    cy.visit('')
+    Helper.testUrl('/login')
   })
 })
--- cypress/support/survey-list-mocks.ts
+++ cypress/support/survey-list-mocks.ts
@@ -1,3 +1,4 @@
 import * as Http from './http-mocks'
 
 export const mockUnexpectedError = (): void => Http.mockServerError('GET', 'surveys')
+export const mockAccessDiniedError = (): void => Http.mockForbiddenError('GET', 'surveys')
