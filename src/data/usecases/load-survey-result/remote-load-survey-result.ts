b707d173f171d157e1731da2ad913592cf61639b
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-26 16:38:02 -0300

    feat: ensure RemoteLoadSurveyResult throws AccessDiniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,10 +1,13 @@
-import { HttpGetClient } from '@/data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccessDiniedError } from '@/domain/errors'
 
 export class RemoteLoadSurveyResult {
   constructor (private readonly url: string,private readonly httpGetClient: HttpGetClient) {}
   async load (): Promise<void> {
-    this.httpGetClient.get({
-      url: this.url
-    })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new AccessDiniedError()
+    }
   }
 }
