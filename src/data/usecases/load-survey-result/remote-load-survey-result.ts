dff47c82758d4d95e61e5114bbc323623d13fe86
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-02-27 20:12:04 -0300

    feat: ensure RemoteLoadSurveyResult return a SurveyResult on 200

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,14 +1,32 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { AccessDiniedError, UnexpectedError } from '@/domain/errors'
+import { LoadSurveyResult } from '@/domain/usecases'
 
-export class RemoteLoadSurveyResult {
-  constructor (private readonly url: string,private readonly httpGetClient: HttpGetClient) {}
-  async load (): Promise<void> {
+export class RemoteLoadSurveyResult implements LoadSurveyResult {
+  constructor (
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Result>) {}
+
+  async load (): Promise<LoadSurveyResult.ResultModel> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
+    const remoteSurveyResult = httpResponse.body
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return Object.assign({}, remoteSurveyResult, { date: new Date(remoteSurveyResult.date) })
       case HttpStatusCode.forbidden: throw new AccessDiniedError()
       default: throw new UnexpectedError()
     }
+  }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Result = {
+    question: string
+    date: string
+    answers: Array<{
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }>
   }
 }
--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -1,16 +1,16 @@
 export interface LoadSurveyResult {
-  load: () => Promise<LoadSurveyResult.ResultModel[]>
+  load: () => Promise<LoadSurveyResult.ResultModel>
 }
 
 export namespace LoadSurveyResult {
   export type ResultModel = {
     question: string
     date: Date
-    answers: [{
+    answers: Array<{
       image?: string
       answer: string
       count: number
       percent: number
-    }]
+    }>
   }
 }
