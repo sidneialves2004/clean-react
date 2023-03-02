1c6959d519f74b16b591be747e1c457275cbc326
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-03-02 09:26:50 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,28 +8,22 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-          <hgroup>
-            <Calendar date={new Date()} className={Styles.calendarWrap} />
-            <h2>Qual é seu framework favorito ?</h2>
-          </hgroup>
-          <FlipMove className={Styles.answerList}>
-            <li>
-              <img src='https://miro.medium.com/v2/resize:fit:640/format:webp/1*cPh7ujRIfcHAy4kW2ADGOw.png' alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li className={Styles.active}>
-              <img src='https://miro.medium.com/v2/resize:fit:640/format:webp/1*cPh7ujRIfcHAy4kW2ADGOw.png' alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li>
-              <img src='https://miro.medium.com/v2/resize:fit:640/format:webp/1*cPh7ujRIfcHAy4kW2ADGOw.png' alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-          </FlipMove>
-          <button>Voltar</button>
+          { false &&
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é seu framework favorito ?  twertweQual é seu framework favorito ?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answerList}>
+              <li>
+                <img src='https://miro.medium.com/v2/resize:fit:640/format:webp/1*cPh7ujRIfcHAy4kW2ADGOw.png' alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+        </>
+        }
           { false && <Loading /> }
       </div>
       <Footer />
