8c6fb42eb9ae41164260d77e8a7f4c747deded26
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-03-02 10:11:14 -0300

    fix: move calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -18,6 +18,9 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('footer')).toHaveTextContent(/^Ver resultado$/)
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2023')
   })
 
   test('Should render with correct values', () => {
@@ -29,5 +32,8 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('footer')).toHaveTextContent(/^Ver resultado$/)
+    expect(screen.getByTestId('day')).toHaveTextContent('05')
+    expect(screen.getByTestId('month')).toHaveTextContent('out')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })
