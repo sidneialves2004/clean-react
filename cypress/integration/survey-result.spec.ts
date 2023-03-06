c2d44783b02d8bffc7cc59b1d7eff03c023ab105
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-03-06 11:35:08 -0300

    test: ensure SurveyResult reload on button click

--- cypress/integration/survey-result.spec.ts
+++ cypress/integration/survey-result.spec.ts
@@ -4,6 +4,24 @@ import * as Http from '../utils/http-mocks'
 
 const path = /api\/surveys/
 
+const surveyResult = {
+  question: 'Question 1',
+  date: '2018-02-03T00:00:00',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer',
+    count: 2,
+    percent: 70,
+    isCurrentAccountAnswer: true
+  },{
+    answer: 'any_answer2',
+    count: 1,
+    percent: 30,
+    isCurrentAccountAnswer: false
+  }]
+}
+
+const mockSuccess = (): void => Http.mockOk(path, 'GET', surveyResult, 1000)
 const mockUnexpectedError = (): void => Http.mockServerError(path,'GET')
 
 describe('SurveyResult', () => {
@@ -15,5 +33,15 @@ describe('SurveyResult', () => {
     mockUnexpectedError()
     cy.visit('/surveys/any_id/results')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+  })
+
+  it('should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id/results')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.wait('@mockOk')
+    cy.getByTestId('question').should('exist')
   })
 })
