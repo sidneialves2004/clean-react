c1e71d69b5634206afcc981c2ccd236391741787
Sidnei Alves <sidneialves2004@yahoo.com.br>
2023-03-08 09:53:42 -0300

    test: ensure SurveyResult present correct data on success

--- cypress/integration/survey-result.spec.ts
+++ cypress/integration/survey-result.spec.ts
@@ -6,7 +6,7 @@ const path = /api\/surveys/
 
 const surveyResult = {
   question: 'Question 1',
-  date: '2018-02-03T00:00:00',
+  date: new Date('2018-02-03T00:00:00'),
   answers: [{
     image: 'any_image',
     answer: 'any_answer',
@@ -50,5 +50,28 @@ describe('SurveyResult', () => {
     mockAccessDiniedError()
     cy.visit('/surveys/any_id')
     Helper.testUrl('/login')
+  })
+
+  it.only('should present survey items', () => {
+    mockSuccess()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('question').should('have.text', surveyResult.question)
+    cy.getByTestId('day').should('have.text', surveyResult.date.getDate().toString().padStart(2,'0'))
+    cy.getByTestId('month').should('have.text', surveyResult.date.toLocaleString('pt-BR', { month: 'short' }).replace('.',''))
+    cy.getByTestId('year').should('have.text', surveyResult.date.getFullYear())
+
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[data-testid="answer"]').text(), surveyResult.answers[0].answer)
+      assert.equal(li.find('[data-testid="image"]').attr('src'), surveyResult.answers[0].image)
+      assert.equal(li.find('[data-testid="percent"]').text(), `${surveyResult.answers[0].percent}%`)
+      assert.equal(li.css('box-shadow'), 'rgb(148, 54, 96) 0px 0px 3px 2px')
+    })
+
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[data-testid="answer"]').text(), surveyResult.answers[1].answer)
+      assert.equal(li.find('[data-testid="image"]').attr('src'), surveyResult.answers[1].image)
+      assert.equal(li.find('[data-testid="percent"]').text(), `${surveyResult.answers[1].percent}%`)
+      assert.equal(li.css('box-shadow'), 'none')
+    })
   })
 })
